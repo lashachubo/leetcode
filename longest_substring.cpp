05cc61632fb24068a006c1c00426e787edbf7699
lashachubo <lashachubinidz3@gmail.com>
2026-04-02 03:25:27 +0400

    longest substring

--- longest_substring.cpp
+++ longest_substring.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <set>
 
-int find_substring(std::string s1){
-  int* head = &s1;
-  int* tail = &s1;
+int find_substring(std::string str){
+  if(str.empty()) return 0;
+  char* head = &str[0];
+  char* tail = &str[0];
   int curr {1};
-  int len {0};
-  std::unordered_set<char> set;
+  int len {1};
+  std::set<char> set;
 
-  while(tail != s1.end()){
-  tail++;  
-  if(!set.count(tail){
-      head++;
-    }    
-    set.append(tail);
-    
-    if(curr > len) len = curr;
-    
-    
-  }
+  set.insert(*tail);
   
+  while(tail != str.data() + str.size() - 1){
+    tail++;
+    
+    while(set.count(*tail)){
+      set.erase(*head);
+      head++;
+      curr--;
+    }
+    
+    curr++;  
+    set.insert(*tail);
+    if(curr > len) len = curr;
+  }
+
+  std::cout << len << "\n";
+  return len;
 }
 
 
@@ -28,7 +35,7 @@ int main(){
   std::string s2 = "bbbbb";
   std::string s3 = "pwwkew";
 
-  find_substring(s1);
+  find_substring(s3);
   
   return 0;
 }
