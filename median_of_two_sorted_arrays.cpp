457755cf6a20b0460bf1b18f7dc58a6e5c0774c5
lashachubo <lashachubinidz3@gmail.com>
2026-04-08 17:57:41 +0400

    error fix + binary search sorting

--- median_of_two_sorted_arrays.cpp
+++ median_of_two_sorted_arrays.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 double median(std::vector<int> arr1, std::vector<int> arr2){
   std::vector<int> sorted;
   double med;
-  size_t i {0};
+  size_t i {0}, j {0};
 
-  while(arr1[i] && arr2[i]){
-    if(i < arr1.size()) sorted.push_back(arr1[i]);   
-    if(i < arr2.size()) sorted.push_back(arr2[i]);
-    i++;
+  while(i < arr1.size() && j < arr2.size()){
+    arr1[i] < arr2[j] ? sorted.push_back(arr1[i++]) : sorted.push_back(arr2[j++]);
   }
 
-  std::sort(sorted.begin(), sorted.end());
-  for(int& i : sorted){
-    std::cout << i << ", ";
-  } 
+  while(i < arr1.size()) sorted.push_back(arr1[i++]);
+  while(j < arr2.size()) sorted.push_back(arr2[j++]);
 
   if(sorted.size() % 2 != 0){
     return sorted[sorted.size() / 2];
   }
   
-  med = (sorted.size() / 2 + sorted.size() / 2 + 1) / 2.0;
+  med = (sorted[sorted.size() / 2 - 1] + sorted[sorted.size() / 2]) / 2.0;
   std::cout << med << "\n";
   return med;
 }
