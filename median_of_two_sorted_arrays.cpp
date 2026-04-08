4159e582176404a7421a981af18c99ed65a75f2e
lashachubo <lashachubinidz3@gmail.com>
2026-04-08 17:37:31 +0400

    rewrote from memory + no double for loop

--- median_of_two_sorted_arrays.cpp
+++ median_of_two_sorted_arrays.cpp
@@ -2,36 +2,38 @@
 #include <vector>
 #include <algorithm>
 
-int find_median(std::vector<int>& num1, std::vector<int>& num2){
+double median(std::vector<int> arr1, std::vector<int> arr2){
   std::vector<int> sorted;
-  double median;
+  double med;
+  size_t i {0};
 
-  for(size_t i {0}; i < num1.size(); i++){
-    sorted.push_back(num1[i]);
-  }
-  for(size_t i {0}; i < num2.size(); i++){
-    sorted.push_back(num2[i]);
+  while(arr1[i] && arr2[i]){
+    if(i < arr1.size()) sorted.push_back(arr1[i]);   
+    if(i < arr2.size()) sorted.push_back(arr2[i]);
+    i++;
   }
 
   std::sort(sorted.begin(), sorted.end());
-  int mid = sorted.size() / 2;
+  for(int& i : sorted){
+    std::cout << i << ", ";
+  } 
 
-  if(sorted.size() % 2 == 0){
-    median = (sorted[mid] + sorted[mid - 1]) / 2.0;
-  } else {
-    median = sorted[mid];  
+  if(sorted.size() % 2 != 0){
+    return sorted[sorted.size() / 2];
   }
-  std::cout << "\n" << sorted[sorted.size() / 2] << " " << sorted[(sorted.size() / 2) - 1] << "\n";
-  std::cout << median;
-  return median;
+  
+  med = (sorted.size() / 2 + sorted.size() / 2 + 1) / 2.0;
+  std::cout << med << "\n";
+  return med;
 }
 
 
 int main(){
-  std::vector<int> num1 = {1, 2};
-  std::vector<int> num2 = {3, 4};
+  std::vector<int> arr1 = {1, 2};
+  std::vector<int> arr2 = {3, 4};
 
-  find_median(num1, num2);
-  
+  median(arr1, arr2);
+
   return 0;
+
 }
